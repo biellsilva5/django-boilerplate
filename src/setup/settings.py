e69e2aad9796d18fe4bdf0e0d1e0425e3be0b1a1
Lucas Silva <lucasgabriel317@gmail.com>
2024-03-23 14:14:23 -0300

    fix static_root for collectstatic

--- src/setup/settings.py
+++ src/setup/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/app/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR ,"static"),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = '/app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
